9e4322a44ac8857d49adb7995dc1189d42d259b3
Luca Baldesi <luca.baldesi@mindmaze.ch>
2020-01-29 14:07:14 +0100

    add simple module hash function

---
+++ src/modulo-hash.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+
+#include "hash-functions.h"
+
+#define PRIME 4294967029
+
+uint32_t modulo_hash(const void* data, int len)
+{
+	const uint_fast8_t* d8 = data;
+	const uint_fast32_t* d32 = data;
+	uint_fast32_t g, h = 0;
+	int i = 0;
+
+	for (; i < len/4; i++)
+		h += d32[i];
+	if (i*4 < len) {
+		g = 0;
+		for (i*=4 ; i < len; i++)
+			g += d8[i] << 8*(len-1-i);
+		h += g;
+	}
+
+	return h%PRIME;
+}
