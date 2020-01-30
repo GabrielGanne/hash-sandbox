e8769804cc341b0b208de43b86458c13808fd0fa
Gabriel Ganne <gabriel.ganne@gmail.com>
2020-01-30 08:29:25 +0100

    enforce coding style

--- src/fibonacci-hash.c
+++ src/fibonacci-hash.c
@@ -9,20 +9,21 @@
  */
 uint32_t fibonacci_32_hash(const void* data, int len)
 {
-	const uint_fast8_t* d8 = data;
-	const uint_fast32_t* d32 = data;
-	uint_fast32_t g, h = 0;
-	int i = 0;
+    const uint_fast8_t* d8 = data;
+    const uint_fast32_t* d32 = data;
+    uint_fast32_t g, h = 0;
+    int i = 0;
 
-	for (; i < len/4; i++)
-		h += (d32[i] * FIBO_NUM) >> 32;
-	if (i*4 < len) {
-		g = 0;
-		for (i*=4 ; i < len; i++)
-			g += d8[i] << 8*(len-1-i);
+    for ( ; i < len / 4 ; i++)
+        h += (d32[i] * FIBO_NUM) >> 32;
 
-		h += (g * FIBO_NUM) >> 32;
-	}
+    if (i * 4 < len) {
+        g = 0;
+        for (i *= 4 ; i < len ; i++)
+            g += d8[i] << 8 * (len - 1 - i);
 
-	return h;
+        h += (g * FIBO_NUM) >> 32;
+    }
+
+    return h;
 }
--- src/modulo-hash.c
+++ src/modulo-hash.c
@@ -6,19 +6,21 @@
 
 uint32_t modulo_hash(const void* data, int len)
 {
-	const uint_fast8_t* d8 = data;
-	const uint_fast32_t* d32 = data;
-	uint_fast32_t g, h = 0;
-	int i = 0;
+    const uint_fast8_t* d8 = data;
+    const uint_fast32_t* d32 = data;
+    uint_fast32_t g, h = 0;
+    int i = 0;
 
-	for (; i < len/4; i++)
-		h += d32[i];
-	if (i*4 < len) {
-		g = 0;
-		for (i*=4 ; i < len; i++)
-			g += d8[i] << 8*(len-1-i);
-		h += g;
-	}
+    for ( ; i < len / 4 ; i++)
+        h += d32[i];
 
-	return h%PRIME;
+    if (i * 4 < len) {
+        g = 0;
+        for (i *= 4 ; i < len ; i++)
+            g += d8[i] << 8 * (len - 1 - i);
+
+        h += g;
+    }
+
+    return h % PRIME;
 }
